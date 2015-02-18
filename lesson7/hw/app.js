1a575684e04441492fcba626497137d94168f5bd
AdrianMLin <adrian.mx.lin@gmail.com>
2015-02-18 18:59:12 -0500

    got some logic working for angular contact list

--- lesson7/hw/app.js
+++ lesson7/hw/app.js
@@ -2,16 +2,31 @@ angular.module('contactList', [])
 
 .directive('contactItem', function(){
 	return {
-		restrict: 'A',
-		templateUrl: './contact-item.html',
+		template: '<section><h3>Contact Item</h3></section>',
+		// templateUrl: './contact-item.html', //why doesn't this work?
 		controller: 'ContactController',
-		controllerAs: 'c'
+		controllerAs: 'c',
+		contact: "=",
+		bindToController: true
 	} 
 })
 
 .controller('FormController', function(){
 	var self = this;
+
+	self.contacts = [];
+
+	self.newContact = {
+		firstName: "",
+		lastName: "",
+		description: "",
+		telephoneNumber: "",
+		email: ""
+	}
+
 	self.addContact = function(){
+
+		self.contacts.push( self.newContact );
 
 	};
 
